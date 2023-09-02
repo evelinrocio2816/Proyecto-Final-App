0dd1f3ee3df21f2a77c2ced3eea3780bfbee1799
Evelin Romero <Evelinrocio2816@gmail.com>
2023-09-02 17:20:19 -0300

    Lista optimizada

--- App.js
+++ App.js
@@ -1,20 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import React, { useState } from 'react'
+
+import Modal from './components/Modal'
+import AntDesing from "@expo/vector-icons/AntDesign";
 
 export default function App() {
+  const [textValue, setTextValue] = useState('')
+  const [itemsList, setItemsList] = useState([])
+  const [itemSelected, setItemSelected] = useState()
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const onHandleChangeItem = text => setTextValue(text)
+
+  const addItem = () => {
+    if (textValue === '') {
+      return
+    }
+    console.log('ejecuta la funcion de agregar elemnto')
+    setItemsList(prevState => [
+      ...prevState,
+      { id: Math.random(), value: textValue },
+    ])
+    setTextValue('')
+  }
+
+  const renderListItem = ({ item, index }) => (
+    <TouchableOpacity style={styles.itemContainer}
+      onPress={() => onHandleModal(index)}>
+      <Text style={styles.textItem}>{item?.value}</Text>
+      <AntDesing name='delete' size={20} color={'white'}/>
+    
+    </TouchableOpacity>
+  )
+
+  const onHandleDelete = () => {
+    console.log(itemSelected)
+    let arr = itemsList
+    arr.splice(itemSelected, 1)
+    setItemsList(arr)
+    setModalVisible(false)
+  }
+
+  const onHandleModal = index => {
+    setModalVisible(true)
+    setItemSelected(index)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Shopping Evelin</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search 🔍 "
+          value={textValue}
+          onChangeText={onHandleChangeItem}
+        />
+        <Button title=" + " color={'#548556'} onPress={addItem} />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={itemsList}
+          renderItem={renderListItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <Modal modalVisible={modalVisible} onHandleDelete={onHandleDelete} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    paddingTop: 80,
+    backgroundColor: '#e2c6ab',
   },
-});
+  title: {
+    fontSize: 35,
+    fontWeight: '500',
+    marginBottom: 25,
+  },
+  inputContainer: {
+    borderRadius: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    paddingRight:10
+  },
+  input: {
+    width: 200,
+    height: 50,
+    fontSize: 17,
+    paddingLeft: 12,
+  },
+  listContainer: {
+    marginTop: 25,
+  },
+  itemContainer: {
+    height: 40,
+    marginVertical: 10,
+    marginHorizontal: 5,
+    borderRadius: 10,
+    justifyContent: 'center',
+    backgroundColor: '#6c9e6d',
+    shadowColor: '#fe6855',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 5,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:10,
+    paddingBottom:0,
+  },
+  textItem: {
+    fontSize: 18,
+    paddingLeft: 15,
+    color: '#fff',
+    fontWeight: '600',
+    fontVariant: 'no-common-ligatures',
+  
+  },
+})
---
+++ components/Modal.js
@@ -0,0 +1,67 @@
+import { Button, Modal as NewModal, StyleSheet, Text, View} from 'react-native'
+
+import React from 'react'
+
+const Modal = ({ modalVisible, onHandleDelete}) => {
+  return (
+    <NewModal visible={modalVisible} animationType="slide" transparent={true}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <View style={styles.modalTitle}>
+            <Text>⚠️</Text>
+          </View>
+          <View style={styles.modalMessage}>
+            <Text>Estas Seguro que quieres eLiminar⁉️</Text>
+          </View>
+          <View style={styles.modalButton}>
+            <Button title="Aceptar"color={'#548556'} onPress={onHandleDelete} />
+            
+            <Button title="Cancelar" color={'#548556'} />
+          
+          </View>
+        </View>
+      </View>
+    </NewModal>
+  )
+}
+
+export default Modal
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  modalContent: {
+    backgroundColor: '#b3e7b3',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  modalTitle: {
+    
+    color: '#FFFFFF',
+    fontSize: 8,
+  },
+  modalMessage: {
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalButton: {
+    marginTop: 15,
+    flexDirection:'row',
+    gap:30
+    
+    
+    
+    
+  },
+})
